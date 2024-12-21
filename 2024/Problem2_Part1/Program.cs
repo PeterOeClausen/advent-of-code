6723ea07ec5ca4e5505c0614e3c110dd08dfbeda
Peter Øvergård Clausen <peteroeclausen@gmail.com>
2024-12-21 16:22:42 +0100

    2024 Problem 2 - Part 1

---
+++ 2024/Problem2_Part1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Problem2_Part1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // var fileName = "sample_input.txt";
+            var fileName = "puzzle_input.txt";
+            var reports = File.ReadLines(fileName);
+
+            Func<int, int, bool> isIncreasing = (x, y) => x < y;
+            Func<int, int, bool> isDecreasing = (x, y) => x > y;
+            Func<int, int, bool> differenceIsAtMostThree = (x, y) => Math.Abs(x - y) <= 3;
+
+            var safeReportCount = 0;
+            foreach (var report in reports)
+            {
+                var levels = report
+                    .Split(" ")
+                    .Select(x => int.Parse(x))
+                    .ToList();
+
+                bool isStillDecreasingRequirement = true;
+                bool isStillIncreasingRequirement = true;
+
+                for (int i = 0; i < levels.Count - 1; i++)
+                {
+                    if (isStillDecreasingRequirement == false && isStillIncreasingRequirement == false)
+                    {
+                        break; // Report is not safe, no need to continue
+                    }
+                    var currentLevel = levels[i];
+                    var nextLevel = levels[i + 1];
+
+                    isStillDecreasingRequirement &= isDecreasing(currentLevel, nextLevel) && differenceIsAtMostThree(currentLevel, nextLevel);
+                    isStillIncreasingRequirement &= isIncreasing(currentLevel, nextLevel) && differenceIsAtMostThree(currentLevel, nextLevel);
+                }
+
+                if (isStillDecreasingRequirement || isStillIncreasingRequirement)
+                {
+                    safeReportCount++;
+                }
+            }
+
+            Console.WriteLine($"Safe report count: {safeReportCount}");
+        }
+    }
+}
