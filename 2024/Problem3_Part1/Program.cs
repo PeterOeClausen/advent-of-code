80a79e56bfe00d8fde0638ef81286f26dc0ba935
Peter Øvergård Clausen <peteroeclausen@gmail.com>
2026-02-21 14:56:56 +0100

    Problem 3 part 1 done.

---
+++ 2024/Problem3_Part1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Problem3_Part1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var fileName = "puzzle_input.txt";
+            if (!File.Exists(fileName))
+            {
+                Console.WriteLine($"File {fileName} does not exist.");
+                return;
+            }
+            var lines = File.ReadLines(fileName);
+            var result = 0;
+
+            Console.WriteLine("Matches found:");
+            foreach (var line in lines)
+            {
+                var matches = Regex.Matches(line, @"mul\(\d+,\d+\)");
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine(match.Value);
+                    var numbers = match.Value
+                        .Replace("mul(", "")
+                        .Replace(")", "")
+                        .Split(",")
+                        .Select(int.Parse)
+                        .ToArray();
+
+                    result += numbers[0] * numbers[1];
+                }
+            }
+            Console.WriteLine("\nFinal result:");
+            Console.WriteLine(result);
+        }
+    }
+}
+
